3c84faa9ad51e0e97f897c47ace2dbf1bfaae529
SaidHadad <saiddavid.hadad@gmail.com>
2021-11-01 23:17:09 -0400

    tetris using react
    the main focus for this game was the state management with different hooks and custom hooks!

---
+++ src/components/Cell.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyledCell } from './styles/StyledCell'
+import { TETROMINOS } from "../utils/tetrominos";
+
+function Cell({ type }) {
+  return (
+    <StyledCell type={type} color={TETROMINOS[type].color} />
+  )
+}
+
+export default React.memo(Cell);
+
